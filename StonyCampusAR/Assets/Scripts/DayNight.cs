53f7d02d90ebdbf69471ed44cfb2564e516b9170
Lawrence-WorkStation <lawrence.yip@stonybrook.edu>
2018-02-03 22:33:17 -0500

    Added sun/moon sprites, began additional tracking handlers

---
+++ StonyCampusAR/Assets/Scripts/DayNight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+
+
+    public float speed;
+    public GameObject light;
+    public GameObject sun;
+    public GameObject moon;
+
+    public float distanceOffset;
+
+
+	// Use this for initialization
+	void Start () {
+        sun.transform.Translate(new Vector3(distanceOffset,0,0));
+        moon.transform.Translate(new Vector3(distanceOffset, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        transform.Rotate(0, 0, speed * Time.deltaTime);
+	}
+}
---
+++ StonyCampusAR/Assets/Scripts/MyTracker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTracker : DefaultTrackableEventHandler {
+
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        GameObject nearest = FindNearestBuilding();
+        //nearest.GetComponentInChildren<Building>().DisplayDave();
+        Debug.Log(nearest.name);
+    }
+
+    private GameObject FindNearestBuilding()
+    {
+        GameObject nearest = new GameObject();
+        GameObject[] BuildingGameObjects = GameObject.FindGameObjectsWithTag("CampusBuildings");
+
+        float closestDist = Mathf.Infinity;
+        foreach (GameObject i in BuildingGameObjects)
+        {
+            Vector3 direction = this.transform.position - i.transform.position;
+            float dist = direction.sqrMagnitude;
+            if (dist < closestDist)
+            {
+                closestDist = dist;
+                nearest = i;
+            }
+        }
+        Debug.Log(this.transform.position);
+        Debug.Log(nearest.transform.position);
+        return nearest;
+    }
+}
