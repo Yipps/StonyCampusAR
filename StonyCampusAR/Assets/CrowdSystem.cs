e5868d36ab9f87a0805f5c79029a16467a742cef
Lawrence-WorkStation <lawrence.yip@stonybrook.edu>
2018-02-07 13:02:19 -0500

    Crowd System: Students now correctly "enter" buildings. Disable for a few second then enabled and moves towards next building

--- StonyCampusAR/Assets/CrowdSystem.cs
+++ StonyCampusAR/Assets/CrowdSystem.cs
@@ -5,17 +5,40 @@ using UnityEngine.AI;
 
 public class CrowdSystem : MonoBehaviour {
 
+    public static CrowdSystem instance = null;
+
     public GameObject student;
     public Transform[] spawnLocations;
     public int studentCount;
     public int numDestinations;
 
+    private int currStudentCount;
+
     GameObject[] buildings;
 
-    // Use this for initialization
+    private void Awake()
+    {
+
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Start () {
+        currStudentCount = 0;
         buildings = BuildingManager.instance.buildingGameObjects;
-        InitStudents();
+        Init();
 	}
 
     public Vector3[] RandSchedule(int count)
@@ -31,22 +54,43 @@ public class CrowdSystem : MonoBehaviour {
         return targets.ToArray();
     }
 
-    public void InitStudents()
+    public void Init()
     {
-        for(int x = 0; x < studentCount; x++)
+        InvokeRepeating("SpawnStudent", 0f, 0.5f);
+    }
+
+    public void SpawnStudent()
+    {
+        Transform randSpawn = spawnLocations[Random.Range(0, spawnLocations.Length)];
+        GameObject _student = Instantiate(student, randSpawn.position, Quaternion.identity);
+        StudentAI _studentAI = _student.GetComponent<StudentAI>();
+        _studentAI.agent.Warp(randSpawn.position);
+        Vector3[] schedule = RandSchedule(numDestinations);
+        _studentAI.schedule = schedule;
+        _studentAI.Init();
+        currStudentCount++;
+        if (currStudentCount == studentCount)
         {
-            Transform randSpawn = spawnLocations[Random.Range(0, spawnLocations.Length)];
-            GameObject _student = Instantiate(student, randSpawn.position, Quaternion.identity);
-            StudentAI _studentAI = _student.GetComponent<StudentAI>();
-            _studentAI.agent.Warp(randSpawn.position);
-            Vector3[] schedule = RandSchedule(numDestinations);
-            _studentAI.schedule = schedule;
-            _studentAI.Init();
+            CancelInvoke();
+            Debug.Log(currStudentCount);
         }
     }
 	
-	// Update is called once per frame
-	void Update () {
+    public IEnumerator ToggleStudent(GameObject student, float time)
+    {
+        student.SetActive(false);
+        yield return new WaitForSeconds(5f);
+        student.SetActive(true);
+        student.GetComponent<StudentAI>().NextTarget();
+    }
+
+    public void StartToggleStudent(GameObject student, float time)
+    {
+        StartCoroutine(ToggleStudent(student, time));
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
--- StonyCampusAR/Assets/StudentAI.cs
+++ StonyCampusAR/Assets/StudentAI.cs
@@ -17,22 +17,31 @@ public class StudentAI : MonoBehaviour {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    public void Init () {
-        currTarget = 0;  
+    public void Init() {
+        currTarget = 0;
         agent.destination = schedule[currTarget];
         moving = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (moving)
-        {
+        
             if (!agent.pathPending && !agent.hasPath)
-            {
-                if (currTarget == schedule.Length)
-                    Object.Destroy(this);
-                agent.destination = schedule[currTarget++];
-            }
+                CrowdSystem.instance.StartToggleStudent(gameObject, 3f);
+        
+    }
+
+    public void NextTarget()
+    { 
+        currTarget++;
+        Debug.Log(currTarget);
+        if (currTarget == schedule.Length)
+        {
+            Debug.Log("die");
+            Object.Destroy(gameObject);
         }
-	}
+            
+        agent.destination = schedule[currTarget];
+    }
 }
