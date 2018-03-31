0a84f8d327d764b81858dfbcbafe55681a108eee
Lawrence-Laptop <Lawrence.yip@stonybrook.edu>
2018-03-31 14:19:29 -0400

    Creating animator controller for all buildings

---
+++ StonyCampusAR/Assets/animationControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class animationControl : MonoBehaviour {
+
+    public bool isWaiting;
+    Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        isWaiting = false;
+	}
+	
+    IEnumerator Playloop()
+    {
+        Debug.Log("Loop");
+        isWaiting = true;
+        animator.SetTrigger("spawn");
+        yield return new WaitForSeconds(3);
+        isWaiting = false;
+       
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (!isWaiting)
+            StartCoroutine(Playloop());
+    }
+}
