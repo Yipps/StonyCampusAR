624321b2db79fec7daa63ac141c3f84d1d8ebb17
Lawrence-Laptop <Lawrence.yip@stonybrook.edu>
2018-04-04 21:44:05 -0400

    Creating pluggable AIs with scriptable objects

---
+++ StonyCampusAR/Assets/ScriptableObject/CoreAI.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CoreAI : ScriptableObject {
+
+    public virtual void Init(StudentAIController ai) { }
+    public abstract void Think(StudentAIController ai);
+}
---
+++ StonyCampusAR/Assets/Scripts/AI/StudentAIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StudentAIController : MonoBehaviour {
+
+    public CoreAI ai;
+    public NavMeshAgent agent;
+    public bool isActive;
+
+
+    private void OnEnable()
+    {
+        ai.Init(this);
+    }
+
+    void Update () {
+        ai.Think(this);
+	}
+}
